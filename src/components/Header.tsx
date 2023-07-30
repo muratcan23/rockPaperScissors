1403599fa97f5af51304dc1ffd9932e693069765
Murat Can <muratal0606@gmail.com>
2023-07-30 14:03:40 +0300

    add tooltip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, Flex, HStack, Text } from "@chakra-ui/react";
+import { ChakraProvider, Flex, HStack, Text, Tooltip } from "@chakra-ui/react";
 
 const Header = () => {
   return (
@@ -11,37 +11,66 @@ const Header = () => {
         bg="#381452"
       >
         <HStack spacing="10%">
-          <Flex
-            border="1px solid red"
-            _hover={{ bg: "gray", cursor: "pointer" }}
-            borderRadius="15%"
-            p="3px"
+          <Tooltip
+            hasArrow
+            label="Rock smashes the scissors !"
+            bg="red.600"
+            fontSize="2xl"
+            borderRadius="15px"
+            alignItems="center"
+            justifyContent="center"
           >
-            <Text fontFamily="cursive" color="white" letterSpacing="2px">
-              Rock
-            </Text>
-          </Flex>
-          <Flex
-            border="1px solid white"
-            _hover={{ bg: "gray", cursor: "pointer" }}
-            borderRadius="15%"
-            p="3px"
+            <Flex
+              border="1px solid red"
+              _hover={{ bg: "gray", cursor: "pointer" }}
+              borderRadius="15%"
+              p="3px"
+            >
+              <Text fontFamily="cursive" color="white" letterSpacing="2px">
+                Rock
+              </Text>
+            </Flex>
+          </Tooltip>
+          <Tooltip
+            hasArrow
+            label="Paper covers the rock !"
+            bg="red.600"
+            fontSize="2xl"
+            borderRadius="15px"
+            alignItems="center"
+            justifyContent="center"
           >
-            <Text fontFamily="cursive" color="white" letterSpacing="2px">
-              Paper
-            </Text>
-          </Flex>
-
-          <Flex
-            border="1px solid yellow"
-            _hover={{ bg: "gray", cursor: "pointer" }}
-            borderRadius="10%"
-            p="3px"
+            <Flex
+              border="1px solid white"
+              _hover={{ bg: "gray", cursor: "pointer" }}
+              borderRadius="15%"
+              p="3px"
+            >
+              <Text fontFamily="cursive" color="white" letterSpacing="2px">
+                Paper
+              </Text>
+            </Flex>
+          </Tooltip>
+          <Tooltip
+            hasArrow
+            label="Scissors cut the paper !"
+            bg="red.600"
+            fontSize="2xl"
+            borderRadius="15px"
+            alignItems="center"
+            justifyContent="center"
           >
-            <Text fontFamily="cursive" color="white" letterSpacing="2px">
-              Scissors
-            </Text>
-          </Flex>
+            <Flex
+              border="1px solid yellow"
+              _hover={{ bg: "gray", cursor: "pointer" }}
+              borderRadius="10%"
+              p="3px"
+            >
+              <Text fontFamily="cursive" color="white" letterSpacing="2px">
+                Scissors
+              </Text>
+            </Flex>
+          </Tooltip>
         </HStack>
       </Flex>
     </ChakraProvider>
